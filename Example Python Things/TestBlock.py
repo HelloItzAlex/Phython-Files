7ecaecfd960b38296dea113569aa7e585a8a6c39
HelloItzAlex <alexaarhus5@gmail.com>
2023-10-04 15:32:34 -0700

    Update TestBlock.py

--- Example Python Things/TestBlock.py
+++ Example Python Things/TestBlock.py
@@ -21,7 +21,7 @@ serialCom.setDTR(False)
 time.sleep(1)
 serialCom.flushInput()
 serialCom.setDTR(True)
-time.sleep(10)
+time.sleep(5)
 
 writer = csv.writer(f, delimiter=",")
 # How many data points to record
@@ -31,16 +31,16 @@ for k in range(kmax):
     try:
         print("bruh1")
         serialCom.write(b'A')
-        #s_bytes = serialCom.readline()
-        #decoded_bytes = s_bytes.decode("utf-8").strip('\r\n')
-        #decoded_bytes = int(decoded_bytes)
-        #writer.writerow([decoded_bytes])
+        s_bytes = serialCom.readline()
+        decoded_bytes = s_bytes.decode("utf-8").strip('\r\n')
+        decoded_bytes = int(decoded_bytes)
+        writer.writerow([decoded_bytes])
 
-        #if ((prev_byte+1) != decoded_bytes):
-            #print('bruh momement')
-            #print(prev_byte,'-', decoded_bytes)
+        if ((prev_byte+1) != decoded_bytes):
+            print('bruh momement')
+            print(prev_byte,'-', decoded_bytes)
 
-        #prev_byte = decoded_bytes
+        prev_byte = decoded_bytes
 
     except:
         print("Error")
