91003e852c8101829db263649a1ef988a4ced676
HelloItzAlex <alexaarhus5@gmail.com>
2023-10-04 08:45:08 -0700

    Python Work

--- Example Python Things/TestBlock.py
+++ Example Python Things/TestBlock.py
@@ -14,32 +14,33 @@ f = open("data.csv","w",newline='')
 f.truncate()
 
 # Open the serial com
-serialCom = serial.Serial('COM19',115200)
+serialCom = serial.Serial('COM16',115200)
 
 # Toggle DTR to reset the Arduino
 serialCom.setDTR(False)
 time.sleep(1)
 serialCom.flushInput()
 serialCom.setDTR(True)
+time.sleep(10)
 
 writer = csv.writer(f, delimiter=",")
-
 # How many data points to record
-kmax = 200
+kmax = 1
 prev_byte = 0; 
 for k in range(kmax):
     try:
         print("bruh1")
-        s_bytes = serialCom.readline()
-        decoded_bytes = s_bytes.decode("utf-8").strip('\r\n')
-        decoded_bytes = int(decoded_bytes)
-        writer.writerow([decoded_bytes])
+        serialCom.write(b'A')
+        #s_bytes = serialCom.readline()
+        #decoded_bytes = s_bytes.decode("utf-8").strip('\r\n')
+        #decoded_bytes = int(decoded_bytes)
+        #writer.writerow([decoded_bytes])
 
-        if ((prev_byte+1) != decoded_bytes):
-            print('bruh momement')
-            print(prev_byte,'-', decoded_bytes)
+        #if ((prev_byte+1) != decoded_bytes):
+            #print('bruh momement')
+            #print(prev_byte,'-', decoded_bytes)
 
-        prev_byte = decoded_bytes
+        #prev_byte = decoded_bytes
 
     except:
         print("Error")
